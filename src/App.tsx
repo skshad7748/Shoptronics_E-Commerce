37126dac7c034b445f059bc2549c162991248a98
Sk Shad <hasibul15-7748@diu.edu.bd>
2025-10-22 00:06:47 +0600

    react setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="font-sans border-2">The quick brown fox ...</p>
+      <p className="font-serif text-2xl">The quick brown fox ...</p>
+      <p className="font-mono text-6xl">The quick brown fox ...</p>
+      <p className="italic text-amber-300">The quick brown fox ...</p>
+    </>
+  );
+}
+
+export default App;

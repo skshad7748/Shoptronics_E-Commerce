de5f4847456cd5c66f9fd18dc651dafa92dea06f
Sk Shad <hasibul15-7748@diu.edu.bd>
2025-11-03 04:37:00 +0600

    live2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Shoptronics_E-Commerce",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       "@atoms": path.resolve(__dirname, "./src/design-system/atoms"),

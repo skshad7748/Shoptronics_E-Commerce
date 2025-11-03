69162700f068e6654cd5bdf8c45e446e791b692f
Sk Shad <139712719+skshad7748@users.noreply.github.com>
2025-11-03 19:11:50 +0600

    change path

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import "./App.css";
 import { CartProvider } from "./store/cart";
 
 const router = createBrowserRouter([
-  { path: "/Shoptronics_E-Commerce", element: <HomePage /> },
+  { path: "/", element: <HomePage /> },
   { path: "/shop", element: <ShopPage /> },
   { path: "/product/:slug", element: <ProductDetailPage /> },
   { path: "/cart", element: <CartPage /> },
